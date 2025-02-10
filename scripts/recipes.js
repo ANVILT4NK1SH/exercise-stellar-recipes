95bb774ec0619205f40533711fc4864d3dfb87b9
Shawn <iamshawnhartline@gmail.com>
2025-02-10 17:27:35 -0600

    Cleaned up code

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -2,19 +2,21 @@ showRecipes = (recipes) => {
   const container = document.querySelector(".js-recipes-grid");
   container.innerHTML = "";
 
-  recipes.forEach((recipe) => {
-    container.insertAdjacentHTML("beforeend", `<div class="recipe-container">
+  let htmlTemp = recipes.map(
+		(recipe) => `
+      <div class="recipe-container">
         <img class="recipe-img" src="${recipe.image}" alt="${recipe.title}">
         <h1 class="recipe-title">${recipe.title}</h1>
         <p class="recipe-description">${recipe.description}</p>
         <div class="recipe-details">
           <p class="ingredients">Ingredients: ${recipe.ingredients.join(
-						', ' 
+						", "
 					)}</p>
-          <p class="steps">Steps: ${recipe.steps.join(' ')}</p>
+          <p class="steps">Steps: ${recipe.steps.join(" ")}</p>
         </div>
-      </div>`);
-	});
+      </div>`
+	).join("");
+  container.insertAdjacentHTML("beforeend", htmlTemp);
 };
 
 showRecipes(recipes);
