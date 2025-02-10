da128017d99c584ee649a5b21a4e121038e6bf54
Shawn <iamshawnhartline@gmail.com>
2025-02-10 17:09:46 -0600

    added folder

---
+++ scripts/recipes.js
@@ -0,0 +1,20 @@
+showRecipes = (recipes) => {
+  const container = document.querySelector(".js-recipes-grid");
+  container.innerHTML = "";
+
+  recipes.forEach((recipe) => {
+    container.insertAdjacentHTML("beforeend", `<div class="recipe-container">
+        <img class="recipe-img" src="${recipe.image}" alt="${recipe.title}">
+        <h1 class="recipe-title">${recipe.title}</h1>
+        <p class="recipe-description">${recipe.description}</p>
+        <div class="recipe-details">
+          <p class="ingredients">Ingredients: ${recipe.ingredients.join(
+						', ' 
+					)}</p>
+          <p class="steps">Steps: ${recipe.steps.join(' ')}</p>
+        </div>
+      </div>`);
+	});
+};
+
+showRecipes(recipes);
